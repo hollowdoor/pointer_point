1afc85d4fcad5f5f1c6c6b44308ed50f0fe11c3c
hollowdoor <hollowdoor99@gmail.com>
2015-11-09 16:56:58 -0600

    Complete rewrite.
    Added multiple targets, better position tracking, and speed tracking.
    
    Everything is fine.

--- README.md
+++ README.md
@@ -1,10 +1,218 @@
 pointer-point
 =============
 
-install
--------
+Version 2 of pointer-point is a complete restructure of it's functionality. Scroll down to visit the documentation for version 1.
+
+Install version 2
+-----------------
 
 `npm install pointer-point`
+
+Constructor
+-----------
+
+### pointer(element|selector|array|array like)
+
+Acceptable arguments to the constructor:
+
+-	single element
+-	single selector
+-	array of elements and/or selectors
+-	an array like (dom library) of elements
+
+All elements get all events applied to them. When ever an event fires it will be on the applicable element. This means if you set three elements the `enter` event will fire for one of those element.
+
+The Example
+-----------
+
+```javascript
+var pointer = require('pointer-point'), divs = document.querySelectorAll('div');
+//All of the div elements are added to the pointer.
+var point = pointer(divs);
+//Change the color for any of the divs depending on events.
+point.on('down', function(current, origin){
+    current.style.backgroundColor = 'red';
+});
+point.on('move', function(current, origin){
+    if(this.inside(divs[2])){
+        //Check some stuff when over the second div..
+        console.log('this.h '+this.h);
+        console.log('this.v '+this.v);
+        console.log('this.speedX '+this.speedX);
+    }
+});
+point.on('up', function(current, origin){
+    current.style.backgroundColor = 'violet';
+});
+point.on('leave', function(current, origin){
+    current.style.backgroundColor = 'violet';
+});
+point.on('enter', function(current, origin){
+    current.style.backgroundColor = 'blue';
+    //Sometimes origin is set to null.
+    if(this.down && origin){
+        //The pointer is down.
+        origin.style.backgroundColor = 'green';
+    }
+});
+point.on('stroke', function(current, origin){
+    console.log('stroke');
+});
+
+```
+
+Alternatively pass an array
+---------------------------
+
+```javascript
+//Add specific divs to the pointer.
+var point = pointer([divs[1], divs[3]]);
+```
+
+Or just one element
+-------------------
+
+```javascript
+var point = pointer(divs[0]);
+```
+
+Instance Methods
+----------------
+
+### destroy()
+
+Removes all of the events for the pointer.
+
+### inside(element)
+
+Is this point inside of the element?
+
+### outside(element)
+
+Is this point outside of the element?
+
+### add(element|selector)
+
+Add one element, or one element from a selector to the instance.
+
+### on(event, listener)
+
+Add an event to the point with a listener function.
+
+### off(event, listener)
+
+Remove an event with the specified listener function.
+
+Event listener
+--------------
+
+### listener(current, origin)
+
+All events receive the current element the pointer is over, when the pointer is down.
+
+All events receive the original element where the pointer was down first.
+
+current, or origin can be null depending on the situation.
+
+Events
+------
+
+The term **pointer** refers to a mouse, or touch.
+
+The term **mutual** means an event is dependent on if the pointer is over one of the elements.
+
+Non-mutual events will sometimes have their `current`, `origin` arguments set to null if the pointer is not over an element. Some mutual events will get this effect too depending on where in the DOM the event fired.
+
+This behavior of mutual events is hard to explain, but it is consistent so you shouldn't have a problem figuring out when this is applicable. **mutual** behavior is much like how traditional mouse events work except for the origin argument which only gets passed on `down`, or `enter` if the pointer went down on one of the elements passed to the constructor.
+
+### down
+
+The pointer went down. **mutual**
+
+### up
+
+The pointer went up. up is **mutual**, but can also fire when not over an element if the pointer was down when it was over that element.
+
+### move
+
+The pointer is moving. Unlike a normal `mousemove` this is fired globally.
+
+### leave
+
+The pointer leaves one of the elements. **mutual** even though it's leaving the element.
+
+### enter
+
+The pointer enters one of the elements. **mutual**
+
+### stroke
+
+The pointer is down, moving, and over one of the elements. **mutual**
+
+`stroke` can fire after `down`, and `enter`.
+
+Instance Properties
+-------------------
+
+### x, y
+
+The coordinates of the pointer.
+
+### h, v
+
+When direction is the pointer moving from it's last position. Their values are strings.
+
+h is the horizontal position (left, right). v is the vertical position (up, down).
+
+### down
+
+Is the pointer down.
+
+### up
+
+Is the pointer up.
+
+### speedX
+
+The pointer speed in pixels per second in the x axis.
+
+### speedY
+
+The pointer speed in pixels per second in the y axis.
+
+**speedX, and speedY are experimental. Don't use these if you don't want your stuff to break.**
+
+Instance Properties Effected By Down State
+------------------------------------------
+
+These properties are set to null when the pointer isn't down.
+
+### current
+
+The current element the pointer is over. This can still be set if the pointer went down then is moved outside the element. current is set to null on pointer up.
+
+### origin
+
+Set when the pointer went down over one of the elements passed to the constructor.
+
+origin records the first element where the pointer went down.
+
+### previous
+
+Like origin, but records any elements the pointer leaves.
+
+Caveats
+-------
+
+Internally pointer-point uses events to track pointer position so until the cursor is moved, or there is a touch there will be no x/y positions. This shouldn't be a problem in most situations.
+
+Version 1 documentation
+-----------------------
+
+Install version 1
+-----------------
+
+`npm install pointer-point@1.1.0`
 
 Usage
 -----
--- index.js
+++ index.js
@@ -3,50 +3,111 @@ var Emitter = require('more-events').Emitter;
 git remote add origin https://github.com/hollowdoor/pointer_point.git
 git push -u origin master
 */
-function Point(element){
-    var self = this;
-    if(typeof element === 'string'){
-        this.root = document.querySelector(elemenet);
-    }else{
-        this.root = element;
+
+function LocalDimensions(point, rect){
+    for(var n in rect)
+        setProp(this, n, rect[n]);
+
+    setProp(this, 'x', point.x - rect.left);
+    setProp(this, 'y', point.y - rect.top);
+
+    function setProp(self, name, value){
+        Object.defineProperty(self, name, {
+            value: value
+        });
+    }
+}
+function Point(elements){
+    var self = this, el = [];
+
+    if(typeof elements.length === 'undefined'){
+        elements = [elements];
     }
 
-    this.emitter = new Emitter(this);
-    this.down = false;
-    this.up = true;
-    //Not fired in touch.
-    this.root.addEventListener('mousedown', onDown, false);
-    window.addEventListener('mousemove', onMove, false);
-    this.root.addEventListener('mouseup', onUp, false);
+    for(var i=0; i<elements.length; i++){
+        if(elements[i] !== undefined){
+            if(typeof elements[i] === 'string'){
+                try{
+                    el.push(document.querySelector(e));
+                }catch(err){
+                    throw new Error(e + ' is not a valid selector used by pointer.');
+                }
+            }else{
+                el.push(elements[i]);
+            }
 
-    this.root.addEventListener('touchstart', onDown, false);
+        }
+    }
+
+    var pos = {}, direction = {}, rect, local,
+        lastmousex=-1, lastmousey=-1, timestamp, mousetravel = 0;
+
+    this.emitter = new Emitter(this);
+
+    this.origin = null;
+    this.current = null;
+    this.previous = null;
+
+    window.addEventListener('mousedown', onDown, false);
+    window.addEventListener('mousemove', onMove, false);
+    window.addEventListener("mouseup", onUp, false);
+
+    window.addEventListener('touchstart', onDown, false);
     window.addEventListener('touchmove', onMove, false);
-    this.root.addEventListener('touchend', onUp, false);
+    window.addEventListener('touchend', onUp, false);
 
     function onDown(e){
+
+        toPoint(e);
         self.down = true;
         self.up = false;
-        self.emitter.emit('down', e);
+        if(self.current){
+            self.origin = self.current;
+            self.emitter.emit('down', self.current, local);
+        }
+
     }
 
     function onMove(e){
         toPoint(e);
-        self.emitter.emit('move', e);
-        if(self.down){
-            self.emitter.emit('stroke', e);
-        }
+        self.emitter.emit('move', this.target, local);
     }
 
     function onUp(e){
         self.down = false;
         self.up = true;
-        self.emitter.emit('up', e);
+        if(self.current){
+            self.emitter.emit('up', self.current, local);
+        }
+        self.current = null;
+        self.previous = null;
+        self.origin = null;
     }
 
     function toPoint(event){
         var dot, eventDoc, doc, body, pageX, pageY;
+        var target, newTarget = null, leaving = null;
 
         event = event || window.event; // IE-ism
+        target = event.target || event.srcElement;
+
+        if(target !== self.current){
+            for(var i=0; i<el.length; i++){
+                if(el[i] === target){
+                    newTarget = target;
+                    break;
+                }
+            }
+
+            leaving = self.current;
+            if(newTarget){
+                self.previous = self.current;
+                self.current = newTarget;
+            }
+        }
+
+        rect = self.current ? self.current.getBoundingClientRect() : null;
+
         //Supporting touch
         //http://www.creativebloq.com/javascript/make-your-site-work-touch-devices-51411644
         if(event.targetTouches) {
@@ -73,52 +134,158 @@ function Point(element){
               (doc && doc.clientTop  || body && body.clientTop  || 0 );
         }
 
-        var rect = self.root.getBoundingClientRect();
-        self.pageX = event.pageX;
-        self.pageY = event.pageY;
-        self.overX = null;
-        self.overY = null;
+        if(self.x && self.y){
+            if(event.pageX < self.x)
+                direction.h = 'left';
+            else if(event.pageX > self.x)
+                direction.h = 'right';
+            if(event.pageY < self.y)
+                direction.v = 'up';
+            else if(event.pageY > self.y)
+                direction.v = 'down';
 
-        self.pos = {};
-        self.pos.x = event.pageX;// - rect.left;
-        self.pos.y = event.pageY;// - rect.top;
-
-        self.boxX = self.pos.x - rect.left;
-        self.boxY = self.pos.y - rect.top;
-        if(event.pageX > rect.right){
-            self.overX = event.pageX - rect.right;
+            lastmousex = self.x;
+            lastmousey = self.y;
         }
 
-        if(event.pageY > rect.bottom){
-            self.overY = event.pageY - rect.bottom;
+        pos = {};
+        pos.x = event.pageX;// - rect.left
+        pos.y = event.pageY;// - rect.top;
+
+        local = rect ? new LocalDimensions(self, rect) : {};
+
+        if(leaving && self.outside(leaving)){
+            if(!newTarget)
+                self.current = null;
+            self.emitter.emit('leave', leaving, local);
+
         }
+
+        if(newTarget){
+            self.emitter.emit('enter', self.current, local);
+        }
+
+        if(self.down && self.current){
+            self.emitter.emit('stroke', self.current, local);
+        }
+
     }
+
+    //Get speed
+    //http://stackoverflow.com/questions/6417036/track-mouse-speed-with-js
+    Object.defineProperty(this, 'speedX', {
+        get: function(){
+            var now = Date.now() / 1000;
+            var dt =  now - timestamp;
+            var dx = self.x - lastmousex;
+            timestamp = now;
+            return Math.round(dx / dt);// * 1000);
+        }
+    });
+
+    Object.defineProperty(this, 'speedY', {
+        get: function(){
+            var now = Date.now() / 1000;
+            var dt =  now - timestamp;
+            var dy = self.y - lastmousey;
+            timestamp = now;
+            return Math.round(dy / dt);// * 1000);
+        }
+    });
 
     Object.defineProperty(this, 'x', {
         get: function(){
-            return self.pos.x;
+            return pos.x;
         }
     });
 
     Object.defineProperty(this, 'y', {
         get: function(){
-            return self.pos.y;
+            return pos.y;
         }
     });
 
-    this.destroy = function(){
-        this.root.removeEventListener('mousedown', onDown, false);
-        window.removeEventListener('mousemove', onMove, false);
-        this.root.removeEventListener('mouseup', onUp, false);
+    Object.defineProperty(this, 'h', {
+        get: function(){
+            return direction.h;
+        }
+    });
 
-        this.root.removeEventListener('touchstart', onDown, false);
+    Object.defineProperty(this, 'v', {
+        get: function(){
+            return direction.v;
+        }
+    });
+
+    this.add = function(element){
+        if(typeof element === 'string'){
+            try{
+                el.push(document.querySelector(e));
+            }catch(err){
+                throw new Error(e + ' is not a valid selector, and can\'t be used add to pointer.');
+            }
+        }else if(!element){
+            throw new Error(e + ' can not be added to pointer.');
+        }
+
+        el.push(element);
+    };
+
+    /*
+    Use this some time later when old browsers are no longer in use.
+    //https://hacks.mozilla.org/2012/05/dom-mutationobserver-reacting-to-dom-changes-without-killing-browser-performance/
+    function applyObserver(element){
+        var observer = MutationObserver(function(mutations){
+            mutations.forEach(function(mutation){
+                var c;
+                if(mutation.type === 'childList'){
+                    c = [].slice(mutation.childList);
+                    if((index = c.indexOf(element)) !== -1){
+                        index = el.indexOf(element);
+                        el.splice(index, 0);
+                        if(!el.length){
+                            self.destroy();
+                        }
+                    }
+                }
+            });
+        });
+
+        observer.observe(element.parentNode, {
+            childList: true
+        });
+    }*/
+
+    this.destroy = function(){
+        window.removeEventListener('mousedown', onDown, false);
+        window.removeEventListener('mousemove', onMove, false);
+        window.removeEventListener('mouseup', onUp, false);
+
+        window.removeEventListener('touchstart', onDown, false);
         window.removeEventListener('touchmove', onMove, false);
-        this.root.removeEventListener('touchend', onUp, false);
+        window.removeEventListener('touchend', onUp, false);
+        el = null;
+        self = null;
+        pos = null;
+        direction = null;
     };
 }
 
 Point.prototype = {
     constructor: Point,
+    inside: function(el){
+        if(!el) throw new TypeError('Cannot be inside '+el);
+        if(elementFromPoint(self.x, self.y) === el) return true;
+        return !this.outside(el);
+    },
+    outside: function(el){
+        var rect;
+        if(!el) throw new TypeError('Cannot be outside '+el);
+        if(elementFromPoint(self.x, self.y) !== el) return true;
+        rect = el.getBoundingClientRect();
+        return (this.y > rect.top-1 || this.y < rect.bottom+1 ||
+            this.x > rect.left-1 || this.y < rect.right+1);
+    },
     on: function(event, cb){
         this.emitter.on(event, cb);
     },
@@ -126,6 +293,21 @@ Point.prototype = {
         this.emitter.off(event, cb);
     }
 };
+
+function elementFromPoint(x, y){
+    if(document.getElementFromPoint)
+        return document.getElementFromPoint(x, y);
+    else
+        return document.elementFromPoint(x, y);
+    return null;
+}
+
+function safeObject(src){
+    var obj = {};
+    for(var n in src)
+        obj[n] = src[n];
+    return obj;
+}
 
 module.exports = function(element){
     return new Point(element);
